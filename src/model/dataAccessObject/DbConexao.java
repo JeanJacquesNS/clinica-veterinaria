468b1df66d6b9cbea5e2012cc4b67a89dda825be
JeanJacquesNS <jeanjackns7@gmail.com>
2022-10-25 16:00:48 +0200

    Criando a conexão com o banco de dados

---
+++ src/model/dataAccessObject/DbConexao.java
@@ -0,0 +1,78 @@
+package model.dataAccessObject;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * @author Jean Jacques N. Shimwa
+ * @created 25-10-2022 - 3:53 PM
+ */
+public class DbConexao {
+
+        private String host;
+        private String db;
+        private String user;
+        private String password;
+        private String porta;
+
+        public DbConexao(){
+            this.host="127.0.0.1";
+            this.porta="3306";
+            this.db="Clinica-Veterinaria";
+            this.user="admin";
+            this.password="admin";
+        }
+
+        public String getHost() {
+            return host;
+        }
+
+        public void setHost(String host) {
+            this.host = host;
+        }
+
+        public String getDb() {
+            return db;
+        }
+
+        public void setDb(String db) {
+            this.db = db;
+        }
+
+        public String getUser() {
+            return user;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getPorta() {
+            return porta;
+        }
+
+        public void setPorta(String porta) {
+            this.porta = porta;
+        }
+
+        public static Connection getConnection()throws SQLException,ClassNotFoundException{
+            Class.forName("com.mysql.jdbc.Driver");
+            DbConexao dbConexao=new DbConexao();
+            return DriverManager.getConnection("jdbc:mysql://"+dbConexao.host+":"+dbConexao.porta+"/"+dbConexao.db+
+                    "?user="+dbConexao.user+"&password="+dbConexao.password+"&noAccessToProcedureBodies=true");
+
+        }
+
+
+    }
+
