455dd8fff09259ae7de43e77baf5da61e1a57bfe
JeanJacquesNS <jeanjackns7@gmail.com>
2022-11-07 16:07:18 +0200

    #Improvement: melhorando a tela principal

--- src/view/MenuPrincipal.java
+++ src/view/MenuPrincipal.java
@@ -12,24 +12,36 @@ public class MenuPrincipal extends JFrame {
 
     private JPanel panel1;
     private JMenuBar jmenubar;
-    private JMenu JmenuRegistos;
-    private JMenu JMenuConfiguracoes;
-    private JMenuItem JMenuItemAnimal;
+    private JMenu JMenuRegistos,JSubMenuRegistos;
+    private JMenuItem JMenuItemAnimal,JMenuItemDono,JMenuItemFuncionario;
+
 
     public MenuPrincipal() {
     }
 
     public MenuPrincipal(String titulo) {
         super(titulo);
-        setBounds(100, 100, 450, 250);
+        jmenubar=new JMenuBar();
+        JSubMenuRegistos=new JMenu();
+        JMenuRegistos=new JMenu("Registos");
+        JMenuItemAnimal=new JMenuItem("Animal");
+        JMenuItemDono=new JMenuItem("Dono");
+        JMenuItemFuncionario=new JMenuItem("Funcionario");
+        setBounds(100, 100, 750, 550);
         Container controlHost = getContentPane();
         controlHost.setLayout(new BorderLayout());
         setJMenuBar(jmenubar);
+        JMenuRegistos.add(JMenuItemAnimal);
+        JMenuRegistos.add(JMenuItemDono);
+        JMenuRegistos.add(JMenuItemFuncionario);
+        JSubMenuRegistos.add(JSubMenuRegistos);
+        jmenubar.add(JMenuRegistos);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
 
-
-
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
