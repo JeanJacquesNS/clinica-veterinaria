1f80f5f6487e9d692fd8dc515201af9be522bdc3
JeanJacquesNS <jeanjackns7@gmail.com>
2022-11-07 14:53:16 +0200

    #Improvement: Adicionando o menu na tela principal

--- src/view/MenuPrincipal.java
+++ src/view/MenuPrincipal.java
@@ -7,19 +7,29 @@ import java.awt.*;
  * @author Jean Jacques N. Shimwa
  * @created 03-11-2022 - 1:11 PM
  */
-public class MenuPrincipal extends JFrame{
+public class MenuPrincipal extends JFrame {
 
 
     private JPanel panel1;
+    private JMenuBar jmenubar;
+    private JMenu JmenuRegistos;
+    private JMenu JMenuConfiguracoes;
+    private JMenuItem JMenuItemAnimal;
+
+    public MenuPrincipal() {
+    }
 
     public MenuPrincipal(String titulo) {
         super(titulo);
-        setBounds(100,100,450,250);
+        setBounds(100, 100, 450, 250);
         Container controlHost = getContentPane();
         controlHost.setLayout(new BorderLayout());
+        setJMenuBar(jmenubar);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-    }
+
+
+
 }
